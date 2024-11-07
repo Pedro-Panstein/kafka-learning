eceadf162b59ee2ae611bb5fcc86ed9e772f3889
Pedro-Panstein <phfarof@gmail.com>
2024-11-06 21:29:17 -0300

    Setup kafka consumer and producer ready

---
+++ src/main/java/net/javaguides/springboot_kafka_tutorial/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot_kafka_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+    }
+}
